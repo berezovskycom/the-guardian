619cd76e8507659247e84ff811c21a1574a0eb42
Vladimir Berezovsky <berezovsky@ex.ua>
2018-01-11 13:04:01 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,7 +7,17 @@ request.responseType = 'json';
 request.send();
 
 const totalPages = document.querySelector('.total-pages');
+const acc = document.querySelectorAll('.accordion');
+const panel = document.querySelectorAll('.panel');
+const refreshBtn = document.querySelector('.refresh');
+const btnPrev = document.querySelector('a.prev');
+const btnNext = document.querySelector('a.next');
+const pageNum = document.querySelector('.page-num');
+const countForm = document.querySelector('.count');
+let i;
 
+
+// error message
 const errHandler = function() {
 	if (request.statusText === '') {
 		document.querySelector('main').style.display = 'none';
@@ -20,10 +30,7 @@ const errHandler = function() {
 
 setTimeout(errHandler, 3000);
 
-const acc = document.querySelectorAll('.accordion');
-const panel = document.querySelectorAll('.panel');
-let i;
-
+// short the news
 const truncate = function( n, useWordBoundary ) {
   if (this.length <= n) { return this; }
   let subString = this.substr(0, n-1);
@@ -32,18 +39,50 @@ const truncate = function( n, useWordBoundary ) {
        : subString) + "...";
 };
 
-const refresh = function() {
-	const title = request.response.response['results'];
+// function to work with pseudo elements
+let UID = {
+	_current: 0,
+	getNew: function(){
+		this._current++;
+		return this._current;
+	}
+};
+
+HTMLElement.prototype.pseudoStyle = function(element,prop,value){
+	let _this = this;
+	let _sheetId = "pseudoStyles";
+	let _head = document.head || document.getElementsByTagName('head')[0];
+	let _sheet = document.getElementById(_sheetId) || document.createElement('style');
+	_sheet.id = _sheetId;
+	let className = "pseudoStyle" + UID.getNew();
+	
+	_this.className +=  " "+className; 
+	
+	_sheet.innerHTML += " ."+className+":"+element+"{"+prop+":"+value+"}";
+	_head.appendChild(_sheet);
+	return this;
+};
+
+// load the news
+const loadNews = function() {
+	const title = request.response.response.results;
 
 	for(i = 0; i < acc.length; i++) {
-		acc[i].textContent = title[i].webTitle;
+		acc[i].innerHTML = `<span class="icon"></span>` + title[i].webTitle;
 		panel[i].innerHTML = `<p>${truncate.apply(title[i].blocks.body[0].bodyTextSummary, [256, true])}</p><br><a href="${title[i].webUrl}" class="link">Read full News</a>`;
 	}
 	totalPages.textContent = request.response.response.pages;
+
+	if (pageNum.value === '1' || pageNum.value === request.response.response.pages.toString()) {
+		btnPrev.classList.add('btn-controls--disabled');
+	} else {
+		btnPrev.classList.remove('btn-controls--disabled');
+	}
 };
 
-request.onload = refresh;
+request.onload = loadNews;
 
+// animations
 for (i = 0; i < acc.length; i++) {
 	acc[i].addEventListener('click', function() {
 		this.classList.toggle('active');
@@ -51,30 +90,40 @@ for (i = 0; i < acc.length; i++) {
 		let panel = this.nextElementSibling;
 		if (panel.style.maxHeight) {
 			panel.style.maxHeight = null;
+			transformIcon('-');
 		} else {
 			panel.style.maxHeight = panel.scrollHeight + 'px';
-		}
+			transformIcon('+');
+		};
 	})
 }
 
-const refreshBtn = document.querySelector('.refresh');
+const transformIcon = function(position) {
+	for(i = 0; i < acc.length; i++) {
+		let arrowIcon = document.querySelectorAll('.icon');
 
+		if (position === '+' && acc[i].classList.contains('active')) {
+			arrowIcon[i].pseudoStyle('before', 'transform', 'rotateZ(-120deg)');
+			arrowIcon[i].pseudoStyle('after', 'transform', 'rotateZ(120deg)');
+		}	else if (position === '-') {
+			arrowIcon[i].pseudoStyle('before', 'transform', 'rotateZ(-60deg)');
+			arrowIcon[i].pseudoStyle('after', 'transform', 'rotateZ(60deg)');			
+		}
+	}
+}
+
+// refresh the news
 refreshBtn.addEventListener('click', (e) => {
 	request.open("GET", requestUrl);
 	request.send();
 });
 
-const btnPrev = document.querySelector('.prev');
-const btnNext = document.querySelector('.next');
-const pageNum = document.querySelector('.page-num');
-const countForm = document.querySelector('.count');
-
+// current page notifier
 function currentPageValue() {
 	pageNum.value = request.response.response.currentPage;
 }
 
-setTimeout(currentPageValue, 700);
-
+// get new page
 const getNewPage = function(e) {
 	if (e === "plus") { 
 		pageNum.value++; 
@@ -97,9 +146,9 @@ btnPrev.onclick = () => {
 btnNext.onclick = () => {
 	if (pageNum.value > request.response.response.pages) return;
 	getNewPage("plus");
-	removeDisabled();
 }
 
+// count/edit pages
 countForm.addEventListener('submit', function(e) {
 	e.preventDefault();
 	let newPage = requestUrl.slice(0, -1);
