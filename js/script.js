747903f14e901c094dd96efaa2484da7ec39b9bf
vl <berezovsky@ex.ua>
2018-03-24 13:20:57 +0200

    cleaner code

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 const API_KEY = '00914858-e291-4910-9b4e-47512b633dde';
-let requestUrl = 'https://content.guardianapis.com/search?&show-blocks=body&api-key=' + API_KEY + '&page=1';
+const requestUrl = `https://content.guardianapis.com/search?
+&show-blocks=body&api-key=${API_KEY}&page=1`;
 
 const request = new XMLHttpRequest();
-request.open("GET", requestUrl);
+request.open('GET', requestUrl);
 request.responseType = 'json';
 request.send();
 
@@ -16,6 +17,17 @@ const pageNum = document.querySelector('.page-num');
 const countForm = document.querySelector('.count');
 let i;
 
+<<<<<<< HEAD
+// error message
+const errHandler = () => {
+  if (request.statusText === '') {
+    document.querySelector('main').style.display = 'none';
+    const err = document.createElement('div');
+    err.className = 'error';
+    err.textContent = 'Sorry, we couldn\'t find the news for you. Please try again later.';
+    document.body.appendChild(err);
+  }
+=======
 
 // error message
 const errHandler = function() {
@@ -26,6 +38,7 @@ const errHandler = function() {
 		err.textContent = 'Sorry, we couldn\'t find the news for you. Please try again later.';
 		document.body.appendChild(err);
 	}	
+>>>>>>> 8b348e34493f08a76d35d502216c429aa5e6464d
 };
 
 setTimeout(errHandler, 3000);
@@ -41,6 +54,106 @@ const truncate = function( n, useWordBoundary ) {
 
 // function to work with pseudo elements
 let UID = {
+<<<<<<< HEAD
+  _current: 0,
+  getNew: function(){
+    this._current++;
+    return this._current;
+  }
+};
+
+HTMLElement.prototype.pseudoStyle = function(element,prop,value){
+  let _this = this;
+  let _sheetId = "pseudoStyles";
+  let _head = document.head || document.getElementsByTagName('head')[0];
+  let _sheet = document.getElementById(_sheetId) || document.createElement('style');
+  _sheet.id = _sheetId;
+  let className = "pseudoStyle" + UID.getNew();
+  
+  _this.className +=  " "+className; 
+  
+  _sheet.innerHTML += " ."+className+":"+element+"{"+prop+":"+value+"}";
+  _head.appendChild(_sheet);
+  return this;
+};
+
+// load the news
+const loadNews = () => {
+  const spanIcon = '<span class=\'icon\'></span>';
+  const { results, pages } = request.response.response;
+  const title = results;
+
+  for (i = 0; i < acc.length; i += 1) {
+    acc[i].innerHTML = spanIcon + title[i].webTitle;
+    panel[i].innerHTML =
+      `<p>${truncate.apply(
+        title[i].blocks.body[0].bodyTextSummary, [256, true])}
+      </p>
+      <br>
+      <a href="${title[i].webUrl}" class="link">Read full News</a>`;
+  }
+  totalPages.textContent = pages;
+
+  if (pageNum.value === '1' || pageNum.value === pages.toString()) {
+    btnPrev.classList.add('btn-controls--disabled');
+  } else {
+    btnPrev.classList.remove('btn-controls--disabled');
+  }
+};
+
+request.onload = loadNews;
+
+// animations
+
+const transformIcon = (position) => {
+  for (i = 0; i < acc.length; i += 1) {
+    const arrowIcon = document.querySelectorAll('.icon');
+    if (position === '+' && acc[i].classList.contains('active')) {
+      arrowIcon[i].pseudoStyle('before', 'transform', 'rotateZ(-120deg)');
+      arrowIcon[i].pseudoStyle('after', 'transform', 'rotateZ(120deg)');
+    } else if (position === '-') {
+      arrowIcon[i].pseudoStyle('before', 'transform', 'rotateZ(-60deg)');
+      arrowIcon[i].pseudoStyle('after', 'transform', 'rotateZ(60deg)');   
+    }
+  }
+};
+
+for (i = 0; i < acc.length; i += 1) {
+  acc[i].addEventListener('click', function() {
+    this.classList.toggle('active');
+
+    const panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+      transformIcon('-');
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + 'px';
+      transformIcon('+');
+    }
+  });
+}
+
+// refresh the news
+refreshBtn.addEventListener('click', () => {
+  request.open('GET', requestUrl);
+  request.send();
+});
+
+// get new page
+const getNewPage = (e) => {
+  if (e === 'plus') {
+    pageNum.value += 1;
+  } else if (e === 'minus') {
+    pageNum.value -= 1;
+  } else {
+    pageNum.value += parseInt(e, 16);
+  }
+  let newPage = requestUrl.slice(0, -1);
+  newPage += pageNum.value;
+  request.open('GET', newPage);
+  request.send();
+};
+=======
 	_current: 0,
 	getNew: function(){
 		this._current++;
@@ -137,13 +250,28 @@ const getNewPage = function(e) {
 	request.open("GET", newPage);
 	request.send();
 }
+>>>>>>> 8b348e34493f08a76d35d502216c429aa5e6464d
 
 btnPrev.onclick = () => {
-	if (pageNum.value <= 1) return;
-	getNewPage("minus");
-}
+  if (pageNum.value <= 1) return;
+  getNewPage('minus');
+};
 
 btnNext.onclick = () => {
+<<<<<<< HEAD
+  if (pageNum.value > request.response.response.pages) return;
+  getNewPage('plus');
+};
+
+// count and edit pages
+countForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let newPage = requestUrl.slice(0, -1);
+  newPage += pageNum.value;
+  request.open('GET', newPage);
+  request.send();
+}, false);
+=======
 	if (pageNum.value > request.response.response.pages) return;
 	getNewPage("plus");
 }
@@ -156,3 +284,4 @@ countForm.addEventListener('submit', function(e) {
 	request.open("GET", newPage);
 	request.send();	
 }, false);
+>>>>>>> 8b348e34493f08a76d35d502216c429aa5e6464d
